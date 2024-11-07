b8cdca7362518ac3aebabe7746e5bcd72b3c8631
jorgeav527 <jorgeav527@gmail.com>
2024-11-06 21:35:22 -0500

    feat: dependency injection

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Union, List, Optional, Any
 from pymongo import MongoClient
 
-from fastapi import FastAPI, Path, Query, Body, HTTPException
+from fastapi import FastAPI, Path, Query, Body, HTTPException, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from datetime import datetime
@@ -9,10 +9,15 @@ from bson import ObjectId
 
 app = FastAPI()
 
-MONGODB_URI = "mongodb+srv://jorgeav527:jorgimetro527@cluster0.mn8kdim.mongodb.net/"
-client = MongoClient(MONGODB_URI)
-db = client["fastapi"]
-collection = db["post"]
+
+def get_db():
+    MONGODB_URI = "mongodb+srv://jorgeav527:jorgimetro527@cluster0.mn8kdim.mongodb.net/"
+    try:
+        client = MongoClient(MONGODB_URI)
+        db = client["fastapi"]
+        yield db
+    finally:
+        client.close()
 
 
 class Post(BaseModel):
@@ -39,9 +44,9 @@ def read_root():
 
 
 @app.get("/api/v1/post", response_model=List[Post])
-def read_all_post() -> List[Post]:
+def read_all_post(db=Depends(get_db)) -> List[Post]:
     result = []
-    posts = collection.find()
+    posts = db["post"].find()
     for post in posts:
         result.append(
             Post(
@@ -55,10 +60,10 @@ def read_all_post() -> List[Post]:
 
 
 @app.get("/api/v1/post/{post_id}", response_model=Post)
-def read_one_post(post_id) -> List[Post]:
+def read_one_post(post_id, db=Depends(get_db)) -> List[Post]:
 
     try:
-        post = collection.find_one({"_id": ObjectId(post_id)})
+        post = db["post"].find_one({"_id": ObjectId(post_id)})
     except:
         raise HTTPException(status_code=400, detail="Invalid ID")
 
@@ -74,7 +79,7 @@ def read_one_post(post_id) -> List[Post]:
 
 
 @app.post("/api/v1/post/create", response_model=Post)
-def create_one_post(post: PostCreate) -> Post:
+def create_one_post(post: PostCreate, db=Depends(get_db)) -> Post:
 
     if not post.title:
         raise HTTPException(status_code=400, detail="title is required")
@@ -86,8 +91,8 @@ def create_one_post(post: PostCreate) -> Post:
         "content": post.content,
         "created": created_time,
     }
-    result = collection.insert_one(new_post)
-    created_post = collection.find_one({"_id": result.inserted_id})
+    result = db["post"].insert_one(new_post)
+    created_post = db["post"].find_one({"_id": result.inserted_id})
 
     return Post(
         id=str(created_post["_id"]),
@@ -98,11 +103,11 @@ def create_one_post(post: PostCreate) -> Post:
 
 
 @app.put("/api/v1/post/edit", response_model=Any)
-def edit_one_post(post: PostUpdate) -> Any:
-    exiting_post = collection.find_one({"_id": ObjectId(post.id)})
+def edit_one_post(post: PostUpdate, db=Depends(get_db)) -> Any:
+    exiting_post = db["post"].find_one({"_id": ObjectId(post.id)})
     updated_post_data = {"title": post.title, "content": post.content}
-    collection.update_one({"_id": ObjectId(post.id)}, {"$set": updated_post_data})
-    updated_post = collection.find_one({"_id": ObjectId(post.id)})
+    db["post"].update_one({"_id": ObjectId(post.id)}, {"$set": updated_post_data})
+    updated_post = db["post"].find_one({"_id": ObjectId(post.id)})
     return (
         Post(
             id=str(exiting_post["_id"]),
@@ -120,14 +125,14 @@ def edit_one_post(post: PostUpdate) -> Any:
 
 
 @app.delete("/api/v1/post/delete", response_model=Any)
-def edit_one_post(post_id: str) -> Any:
+def edit_one_post(post_id: str, db=Depends(get_db)) -> Any:
     try:
-        post = collection.find_one({"_id": ObjectId(post_id)})
+        post = db["post"].find_one({"_id": ObjectId(post_id)})
     except:
         raise HTTPException(status_code=400, detail="invalid Id")
 
     if post is None:
         raise HTTPException(status_code=400, detail="post not found")
 
-    collection.delete_one({"_id": ObjectId(post_id)})
+    db["post"].delete_one({"_id": ObjectId(post_id)})
     return {"message": "Post borrado exitosamente"}
